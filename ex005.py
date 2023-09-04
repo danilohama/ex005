5894818087733baa6a641b229993fbfd154cdaad
Danilo ohama <91641703+danilohama@users.noreply.github.com>
2023-09-04 19:29:08 -0300

    Add files via upload

---
+++ ex005.py
@@ -0,0 +1,6 @@
+#  Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor
+
+n = int(input('Digite um numero: '))
+#  a = n - 1 minimizando o tamanho do codigo
+#  s = n + 1
+print('Analisando o valor {}, Seu antecessor é {} e o seu sucessor é {}'.format(n, (n - 1), (n + 1)))
